b505a6ca28150d5dfe13a37997f86d923cc9b5f2
Nate Elliott <chesaman12@gmail.com>
2019-01-28 17:45:58 -0800

    nate_branch

--- Practice/src/Practice.java
+++ Practice/src/Practice.java
@@ -2,5 +2,10 @@
 public class Practice {
 	public static void main(String... args) {
 		System.out.print("Hello Git!");
+		System.out.print(someRandomMethod());
+	}
+	
+	private static String someRandomMethod() {
+		return "\nnate_branch";
 	}
 }
