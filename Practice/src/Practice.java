985f0959734b9c041562b7d6212475a4e0a60fba
Preston Tomes <prestontomes@gmail.com>
2019-01-30 12:16:30 -0800

    Preston branch Change

--- Practice/src/Practice.java
+++ Practice/src/Practice.java
@@ -1,7 +1,7 @@
 
 public class Practice {
 	public static void main(String... args) {
-		System.out.print("Hello Gita!");
+		System.out.print("Hello Gitaaaaa!");
 		System.out.print(someRandomMethod());
 	}
 	
