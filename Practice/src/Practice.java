e55c494c178f9dd00a1f23eac1e1cc631cda9740
Preston Tomes <prestontomes@gmail.com>
2019-01-30 12:13:54 -0800

    Changed A line in the Print

--- Practice/src/Practice.java
+++ Practice/src/Practice.java
@@ -1,7 +1,7 @@
 
 public class Practice {
 	public static void main(String... args) {
-		System.out.print("Hello Git!");
+		System.out.print("Hello Gita!");
 		System.out.print(someRandomMethod());
 	}
 	
